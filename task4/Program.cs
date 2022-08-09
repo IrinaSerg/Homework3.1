05adaa0284ec4003409d973453d4b78fd0c86c3c
Irina Sergeeva <Irinka197308@mail.ru>
2022-08-09 21:55:26 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите первое число: ");
+int a = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Int32.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Int32.Parse(Console.ReadLine());
+
+int max = 0;
+
+Console.Write("Результат: ");
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+if (max > c)
+{
+    Console.Write(max);
+}
+else
+{
+    Console.Write(c);
+}
