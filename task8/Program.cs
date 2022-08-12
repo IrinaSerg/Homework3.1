5f95bfd1014d3e5ccc324267069430b4091e0fca
Irina Sergeeva <Irinka197308@mail.ru>
2022-08-12 17:45:00 +0300

    add task8

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите положительное число: ");
+uint number = Convert.ToUInt32(Console.ReadLine());
+
+for(int i = 1; i <= number; i++)
+{ 
+    if(i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(" ");                            
+    }
+}
+
