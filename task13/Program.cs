68c8ba362848e0c279345d198998ebd2e21cbd0f
Irina Sergeeva <Irinka197308@mail.ru>
2022-08-16 16:16:20 +0300

    add task13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+if(number.Length <= 2)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine(number[2]);
+}
+
+
