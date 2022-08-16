2c634d5b871f86d38ef822a3aecc47976e0e3ac1
Irina Sergeeva <Irinka197308@mail.ru>
2022-08-16 16:59:44 +0300

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите день недели(1-7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+ 
+if(day < 1 || day > 7)
+{
+    Console.WriteLine("Такого дня не существует, введите от 1 до 7");
+    Environment.Exit(1);
+}
+
+if(day <= 5)
+{
+    Console.WriteLine("Будний");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
