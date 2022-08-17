e7c72efb60e958a11cc0b779b7359a4155b431bb
Irina Sergeeva <Irinka197308@mail.ru>
2022-08-17 23:22:25 +0300

    add task21

---
+++ task21/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] convertToInt(string[] sCoords)
+{
+    int[] result = new int[sCoords.Length];
+    for(int i = 0; i < sCoords.Length; i++)
+    {
+        result[i] = Convert.ToInt32(sCoords[i]);
+    }
+    return result;
+}
+
+Console.WriteLine("Введите координаты точки A(x,y,z): ");
+string sCoordsA = Console.ReadLine();
+Console.WriteLine("Введите координаты точки B(x,y,z): ");
+string sCoordsB = Console.ReadLine();
+
+
+int[] coordsA = convertToInt(sCoordsA.Split(","));
+int[] coordsB = convertToInt(sCoordsB.Split(","));
+
+double powX = Math.Pow((coordsB[0] - coordsA[0]), 2);
+double powY = Math.Pow((coordsB[1] - coordsA[1]), 2);
+double powZ = Math.Pow((coordsB[2] - coordsA[2]), 2);
+double sum = powX + powY + powZ;
+double result = Math.Sqrt(sum);
+
+Console.WriteLine($"Результат: {result.ToString("F2")}");
