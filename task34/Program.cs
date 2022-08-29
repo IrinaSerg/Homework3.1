e2ef01b75170c473812a3acc36db86dc5534aacb
Irina Sergeeva <Irinka197308@mail.ru>
2022-08-29 22:12:13 +0300

    add task34

---
+++ task34/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = 4;
+int[] numbers = new int[n];
+
+Random random = new Random();
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(100, 999);
+}
+Console.WriteLine($"Вывод случайных элементов: [{string.Join(",", numbers)}]");
+
+int evenAmount = 0;
+for(int i = 0; i < numbers.Length; i++)
+{
+   if(numbers[i] % 2 == 0) 
+   {
+     evenAmount++;
+   }  
+}
+Console.WriteLine($"Кол-во четных чисел: {evenAmount}");
