c5216580b3df31a81331beaf7b9ddec03237b527
Irina Sergeeva <Irinka197308@mail.ru>
2022-09-01 20:17:14 +0300

    add task38

---
+++ task38/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] numbers = new int[4];
+
+Random random = new Random();
+int min = 100;
+int max = 0;
+for(int i = 0; i < numbers.Length; i++)
+{
+    int number = random.Next(0, 100);
+    if(number < min)
+    {
+        min = number;
+    }
+    if(number > max)
+    {
+        max = number;
+    }
+    numbers[i] = number;
+}
+Console.WriteLine($"Вывод случайных элементов: [{string.Join(",", numbers)}]");
+ 
+Console.WriteLine($"Разница max - min: {max - min}");
