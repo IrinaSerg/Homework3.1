c0798d2948706b6b3f28a27c4947892c0a47c744
Irina Sergeeva <Irinka197308@mail.ru>
2022-09-01 20:49:46 +0300

    add task41

---
+++ task41/Program.cs
@@ -0,0 +1,14 @@
+﻿int counter = 0;
+Console.WriteLine("Введите числа через запятую: ");
+string[] input = Console.ReadLine().Split(",");
+
+for(int i = 0; i < input.Length; i++)
+{
+    int number = Convert.ToInt32(input[i]);
+    if(number > 0)
+    {
+        counter++;
+    }
+}
+
+Console.WriteLine($"Количество чисел больше нуля: {counter}");
