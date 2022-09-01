c342420b6bb84401d88b0e2b03dd35d891ea7a80
Irina Sergeeva <Irinka197308@mail.ru>
2022-09-01 19:47:19 +0300

    add task36

---
+++ task36/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] numbers = new int[4];
+
+Random random = new Random();
+for(int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(-99, 100);
+}
+Console.WriteLine($"Вывод случайных элементов: [{string.Join(",", numbers)}]");
+
+int sum = 0;
+for(int i = 1; i < numbers.Length; i += 2)
+{
+    sum += numbers[i];
+}
+Console.WriteLine($"Сумма нечетных позиций: {sum}");
