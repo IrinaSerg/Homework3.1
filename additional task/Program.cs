3694e197a7b710088001e14492169f202fbb1870
Irina Sergeeva <Irinka197308@mail.ru>
2022-09-06 19:23:22 +0300

    add additional task

---
+++ additional task/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] myArray = new int[3,4];
+for(int i = 0; i < myArray.GetLength(0); i++)
+{
+    for(int j = 0; j < myArray.GetLength(1); j++)
+    {
+        myArray[i,j] = new Random().Next(-10, 11);
+        Console.Write(myArray[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+
+int sum(int[,] myArray)
+{
+    int result = 0;
+    for(int i = 0; i < myArray.GetLength(0); i++)
+    {
+      for(int j = 0; j < myArray.GetLength(1); j++)
+      {
+        if(i == j)
+        {
+            result += myArray[i,j];
+        }
+      }
+    }
+    return result;
+}
+Console.Write($"Сумма элементов главной диагонали: {sum(myArray)}");
+
