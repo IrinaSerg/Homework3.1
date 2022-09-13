9c3584977f57bf0ce6fbcb46f24a9223b1422cf2
Irina Sergeeva <Irinka197308@mail.ru>
2022-09-13 22:51:52 +0300

    add task50

---
+++ task50/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int[,] array = {{1,4,7,2}, {5,9,2,3}, {8,4,2,4}};
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result = "такого числа в массиве нет";
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+        if(array[i,j] == number)
+        {
+            result = $"Индекс - [{i},{j}]";
+            break;
+        }
+    }
+}
+
+Console.WriteLine(result);
