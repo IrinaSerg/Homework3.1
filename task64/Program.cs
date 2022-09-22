d0db9eec87c2fff70b0f04ed6c1bf00cbe484130
Irina Sergeeva <Irinka197308@mail.ru>
2022-09-22 13:23:43 +0300

    add task64

---
+++ task64/Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintRange(int m, int n)
+{
+    if(m > n)
+    {
+        return;
+    }
+    Console.Write($"{m} ");
+    PrintRange(m + 1, n);
+}
+
+
+Console.Write("Введите натуральное число m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintRange(m, n);
