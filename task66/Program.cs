89c73ee0ec5f4433af4e07c026ed799470fb07cd
Irina Sergeeva <Irinka197308@mail.ru>
2022-09-22 14:03:48 +0300

    add task66

---
+++ task66/Program.cs
@@ -0,0 +1,18 @@
+﻿int RangeSum(int m, int n)
+{
+    if(m > n)
+    {
+        return 0;
+    }
+        
+    return m + RangeSum(m + 1, n);
+}
+
+
+Console.Write("Введите натуральное число m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = RangeSum(m, n);
+Console.WriteLine(result);
