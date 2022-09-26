9825b8230e18aac0b8f24fff50d737a60126f986
Irina Sergeeva <Irinka197308@mail.ru>
2022-09-26 19:36:23 +0300

    add task60

---
+++ task60/Program.cs
@@ -0,0 +1,34 @@
+﻿int[,,] FillArray() 
+{
+    int[,,] numbers = new int[2,2,2];
+    Random random = new Random();
+    for(int i = 0; i < numbers.GetLength(0); i++) 
+    {
+        for(int j = 0; j < numbers.GetLength(1); j++)
+        {
+            for(int k = 0; k < numbers.GetLength(2); k++)
+            {
+                numbers[i,j,k] = random.Next(10, 100);
+            }
+        }
+    }
+    return numbers;
+}
+
+void PrintArray(int[,,] numbers)
+{
+    for(int i = 0; i < numbers.GetLength(0); i++) 
+    {
+        for(int j = 0; j < numbers.GetLength(1); j++)
+        {
+            for(int k = 0; k < numbers.GetLength(2); k++)
+            {
+                Console.Write($"{numbers[i,j,k]}({i},{j},{k})");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] numbers = FillArray();
+PrintArray(numbers);
